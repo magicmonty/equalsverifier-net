8a5021926f3435b1e340fbe0e197e852ad7d7fcf
Martin Gondermann <magicmonty@pagansoft.de>
2014-12-05 10:07:15 +0100

    * First passing NonNullity Test * Set code formatting policies

---
+++ EqualsVerifier.Tests/Integration/BasicContract/NonNullityTest.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+using EqualsVerifier.TestHelpers;
+using EqualsVerifier.TestHelpers.Types;
+
+namespace EqualsVerifier.Integration.BasicContract
+{
+    [TestFixture]
+    public class NonNullityTest : IntegrationTestBase
+    {
+        [Test]
+        public void GivenNullInput_WhenNullPointerExceptionIsThrown_ThenFail()
+        {
+            ExpectFailure(
+                () => EqualsVerifier.ForType<NullReferenceExceptionThrower>().Verify(),
+                "Non-nullity: NullReferenceException thrown");
+        }
+
+
+        #pragma warning disable 659
+        sealed class NullReferenceExceptionThrower : Point
+        {
+            public NullReferenceExceptionThrower(int x, int y) : base(x, y)
+            {
+            }
+
+            public override bool Equals(object obj)
+            {
+                return obj.GetType().Equals(GetType()) && base.Equals(obj);
+            }
+        }
+        #pragma warning restore 659
+    }
+}
+
