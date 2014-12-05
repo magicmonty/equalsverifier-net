48d8d151a99bf7539f1cf0b20b6886a51fd4f0b0
Martin Gondermann <magicmonty@pagansoft.de>
2014-12-05 10:35:32 +0100

    First Reflexivity Test

---
+++ EqualsVerifier.Tests/Integration/BasicContract/ReflexivityTest.cs
@@ -0,0 +1,39 @@
+﻿using NUnit.Framework;
+using EqualsVerifier.TestHelpers;
+using EqualsVerifier.TestHelpers.Types;
+
+namespace EqualsVerifier.Integration.BasicContract
+{
+    [TestFixture]
+    public class ReflexivityTest : IntegrationTestBase
+    {
+        [Test]
+        public void WhenReferencesAreNotEqual_ThenFail()
+        {
+            ExpectFailure(
+                () => EqualsVerifier.ForType<ReflexivityIntentionallyBroken>().Verify(),
+                "Reflexivity", "object does not equal itself", typeof(ReflexivityIntentionallyBroken).Name);
+        }
+
+        #pragma warning disable 659
+        sealed class ReflexivityIntentionallyBroken : Point
+        {
+            // Instantiator.scramble will flip this boolean.
+            public bool Broken = false;
+
+            public ReflexivityIntentionallyBroken(int x, int y) : base(x, y)
+            {
+            }
+
+            public override bool Equals(object obj)
+            {
+                if (Broken && this == obj)
+                    return false;
+
+                return base.Equals(obj);
+            }
+        }
+        #pragma warning restore 659
+    }
+}
+
