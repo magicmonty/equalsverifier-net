2b824f02ea600c134d42ae7981ee121f3133d4df
Martin Gondermann <magicmonty@pagansoft.de>
2014-12-06 12:32:59 +0100

    Added DontOverrideEqualsTest

---
+++ EqualsVerifier.Tests/Integration/ExtendedContract/DontOverrideEqualsTest.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+
+namespace EqualsVerifier.Integration.ExtendedContract
+{
+    [TestFixture]
+    public class DontOverrideEqualsTest
+    {
+
+        [Test]
+        public void WhenClassDoesntOverrideEqualsOrHashCode_ThenSucceed()
+        {
+            EqualsVerifier.ForType<Poco>()
+                .Suppress(Warning.NONFINAL_FIELDS)
+                .Verify();
+        }
+
+        public sealed class Poco
+        {
+            string _value;
+
+            public string Value
+            {
+                get { return _value; }
+                set { _value = value; }
+            }
+
+            public override string ToString()
+            {
+                return GetType().Name + " " + (_value ?? string.Empty);
+            }
+        }
+    }
+}
+
